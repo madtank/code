8cf7e13a4ccd8321d246c6109b812aae53802e4c
Jacob Taunton <jandrewt82@gmail.com>
2020-03-08 12:01:25 -0700

    update

--- processlist.py
+++ processlist.py
@@ -8,7 +8,7 @@ accounts = open('accounts.txt','r').read().split('\n')
 
 
 for account in accounts:
-    cmd = "cmd /c set AWS_DEFAULT_PROFILE={}".format(account)
+    cmd = "cmd /k set AWS_DEFAULT_PROFILE={}".format(account)
     os.system(cmd)
      # subprocess.call("set AWS_DEFAULT_PROFILE=account")
-    # print(account)
+    print(account)
